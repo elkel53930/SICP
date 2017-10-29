e2161b839460c46962da2d5b419d02b2090c67c4
elkel53930 <elkel53930@gmail.com>
2017-10-30 08:22:25 +0900

    練習問題1.9を解いてる最中

---
+++ practice1_9.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (inc n)
+  (+ n 1))
+
+(define (dec n)
+  (- n 1))
+
+(define (plus1 a b)
+  (if (= a 0)
+      b
+      (inc (plus1 ( dec a) b))))
+
+(define (plus2 a b)
+  (if (= a 0)
+      b
+      (plus2 (dec a) (inc b))))
+
+(plus1 2 3)
+(plus2 4 5)
