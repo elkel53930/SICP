bdbb7ab5b58016eab768f04f593ba76588cd32b6
elkel53930 <elkel53930@gmail.com>
2017-10-30 07:59:16 +0900

    p31まで読んだ。

---
+++ sqrt2.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (square x) (* x x))
+(define (average x y) (/ (+ x y) 2))
+
+(define (mysqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+(mysqrt 10)
