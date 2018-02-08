8fd7bfc834314b3b8a3f11d8b6ac81334f8fb8c8
elkel53930 <elkel53930@gmail.com>
2018-02-08 23:09:14 +0900

    work out ex2

---
+++ ch3/sec1/ex02.scm
@@ -0,0 +1,17 @@
+(define (make-monitored f)
+  (let ((count 0))
+    (lambda (arg)
+      (cond ((eq? arg 'how-many-calls?) count)
+            ((eq? arg 'reset-count)
+              (begin (set! count 0)
+                     count))
+            (else
+              (begin (set! count (+ count 1))
+                     (f arg)))))))
+
+(define s (make-monitored sqrt))
+(print (s 100))
+(print (s 144))
+(print (s 'how-many-calls?))
+(s 'reset-count)
+(print (s 'how-many-calls?))
